4e5de603a69f4b0d7980cf528c20fe2521db9cd4
Lhau <buarquecode@hotmail.com>
2022-08-24 03:24:19 -0300

    console log do pratica-1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,8 @@
-Nome:
-Email:
-Profissao:
-Idade:
-Local:
+//criar as variaveis com comando let
+//na primeira tentativa nao usei
+let Nome = "Lhau"
+let Email = "buarquecode@hotmail.com"
+let Profissao = "programador"
+let Idade = 100
+let Local = "quinto dos inferno"
+console.log( {Nome, Email, Profissao, Idade, Local} )
