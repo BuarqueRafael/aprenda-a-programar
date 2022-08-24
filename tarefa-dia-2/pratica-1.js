d3c3251066221f24b5fae8aecff6288d67c21c38
Lhau <buarquecode@hotmail.com>
2022-08-24 03:55:55 -0300

    Desafio opcional concluido

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -6,3 +6,5 @@ let Profissao = "programador"
 let Idade = 100
 let Local = "quinto dos inferno"
 console.log( {Nome, Email, Profissao, Idade, Local} )
+let texto =  `Olá, ${Nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${Local}. Confirma pra gente alguns dados? A sua idade é ${Idade}, você é ${Profissao} e o seu email é ${Email}? Ficamos no aguardo. Muito obrigado e boa viagem! D`
+console.log(texto)
